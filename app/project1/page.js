d3d85ac697fef71fa84538e9e728768053de4301
alexjungdev <alexjungdev@gmail.com>
2024-03-08 01:44:52 +0900

    feat: todays-to-do 프로젝트 추가 / images 변경

--- app/project1/page.js
+++ app/project1/page.js
@@ -5,72 +5,55 @@ import { Project } from "@/components/projectComponent";
 import { Container, MainContainer} from '../../components/portfolioStyle'
 
 
-
 export default function Project1() {
 
     const CarouselItems = [
         {
             id: 1,
-            imgUrl: '/PF1.png',
-            webimgUrl: '/PF1.webp',
+            imgUrl: '/WG1.png',
+            webimgUrl: '/WG1.webp',
         },
         {
             id: 2,
-            imgUrl: '/PF2.png',
-            webimgUrl: '/PF2.webp',
+            imgUrl: '/WG2.png',
+            webimgUrl: '/WG2.webp',
         },
         {
             id: 3,
-            imgUrl: '/PF3.png',
-            webimgUrl: '/PF3.webp',
+            imgUrl: '/WG3.png',
+            webimgUrl: '/WG3.webp',
         },
         {
             id: 4,
-            imgUrl: '/PF4.png',
-            webimgUrl: '/PF4.webp',
-        },
-        {
-            id: 5,
-            imgUrl: '/PF5.png',
-            webimgUrl: '/PF5.webp',
-        },
-        {
-            id: 6,
-            imgUrl: '/Performance.png',
-            webimgUrl: '/Performance.webp',
+            imgUrl: '/WG4.png',
+            webimgUrl: '/WG4.webp',
         },
     ];
 
-    return (
-            <Container style={{ backgroundRepeat: 'no-repeat', backgroundSize: '100%', backgroundPosition: 'center' }}>
-                <Project.Title title={"포트폴리오 웹사이트"} type={"Website"} date={"2024-01"} />
-                <MainContainer>
-                    <Project.ImageGallery carousel_items={CarouselItems} />
-                    <Project.Description 
-                    link={"https://alexjung-portfolio.vercel.app/"} 
-                    tech_stack={"React, Next.JS, CSS, JavaScript, Styled-Component, Scss"}
-                    deployment={"Vercel"}
-                    about_project={`프론트엔드 개발자로의 포트폴리오를 목적으로 만든 웹사이트입니다. 이전에 React로 개발한 프로젝트가 SEO에서 메리트가 없다는 것을 체감하고 Next.js로 개발을 진행했습니다.
 
-                    프로젝트에서 컴포넌트 기반의 Framer-Motion이라는 라이브러리를 사용하여 이미지 갤러리 및 버튼 상호작용 등 다양한 애니메이션 모션들을 제어하였습니다. 그리고 Styled-Component를 통해 CSS-in-js의 강한 직관성과 가독성 향상의 편리함을 알게되었습니다.
-                    
-                    여러 해상도에 대응하기 위해 반응형 디자인을 위하여 Scss와 Styled-Component에 각각 Media Query를 적용하였고 Vercel을 이용하여 정적 사이트로 배포 하였습니다.
-             
-                    최종적으로 배포 이후 Lighthouse를 사용하는 PageSpeed Insights를 통해 성능,접근성,검색엔진 최적화를 진단받았고 개선된 사용자 경험을 위해 여러가지 권장 사항들을 참고하여 전면 수정하였습니다.
-                    
-                    최적화 결과
-                    LCP: 2.4s → 1.9s
-                    CLS: 0.439 → 0
+    return (
+        <Container style={{ backgroundRepeat: 'no-repeat', backgroundSize: '100%', backgroundPosition: 'center' }}>
+            <Project.Title title={"글 생성기"} type={"App"} date={"2022-06 ~ 2022-08"} />
+            <MainContainer>
+                <Project.ImageGallery carousel_items={CarouselItems} />
+                <Project.Description
+                    link={"https://apps.apple.com/us/app/writing-generator/id6456040207"}
+                    tech_stack={"React-Native, Css, JavaScript"}
+                    deployment={"Apple App Store"}
+                    about_project={`React-Native를 통해 처음으로 만든 글 생성기 인공지능 AI 앱입니다.
+
+                    Open AI의 GPT를 도입하여 이용자들이 원하는 종류의 글을 만들어주도록 설정하였습니다. 현재 동화, 이야기, 시만 만들 수 있지만 서비스 중인 앱으로 더 많은 장르들이 추가될 예정입니다.
+
+                    주요 기능은 Rest API를 통하여 사용자가 선택한 파라미터들이 포함된 URL를 Azure Function쪽에 POST요청을 보내면 보안되어있는 Azure Key Vault에서 Secret키를 확인한 이후 생성된 결과를 클라이언트 측으로 Return하는 방식으로 구현했습니다.
+
+                    처음 만든 앱이자 처음 광고를 추가한 앱이며 애플 스토어에 출시 이후 꾸준히 이용되고 있다는 점에서 큰 동기부여가 되었던 프로젝트입니다.
                     `}
-                    learned_solved={`1. React에서 SEO를 직접 구현하는 과정은 꽤나 불편합니다. 페이지별 메타태그를 변경하려면 react-helmet-async을 설치해야하고 사전 렌더링을 구현하려면 react-snap이라는 업데이트가 5년 전에 멈춘 라이브러리를 사용해야합니다. 
-                    하지만 Next.js는 기본적으로 SSR,SSG를 제공하기 때문에 초기 렌더링 속도가 빠르며 완성된 페이지를 클라이언트에게 건네주기 때문에 검색 엔진에서 매우 이점이 있습니다. Next.js가 계속해서 인기를 얻는 이유를 몸소 느끼게 되었습니다.
-                   
-                    2. 배포 이후 홈페이지의 성능을 체크해봤더니 CLS에서 큰 성능 하락이 있었습니다. 계속해서 테스트해본 결과 Styled-Component가 그 원인인걸 알아냈습니다.
-                    꽤 많은 외국의 개발자들이 Styled-Component와 같은 CSS-in-js들을 사용하고 큰 성능 하락을 얘기하였고 이를 바탕으로 포트폴리오의 첫번째 화면에 들어간 움직이는 배경화면을 CSS로 옮겼습니다. 즉시 저는 CLS와 LCS 모두 눈에 띄는 수치 변화를 보았습니다.`}
-                    />
-                </MainContainer>
-                <MainContainer />
-            </Container>
+                    learned_solved={`1. 인터넷에 검색해보니 Open Ai의 Api키가 가 외부로 유출되어서 백 달러부터 만 달러까지 사용료가 사용된 사람들이 존재했습니다. 이를 통해 Azure이라는 클라우드 서비스를 이용하기로 했고 Key Vault를 통하여
+                    Api키를 안전하게 보관하였습니다. Api키를 절대 클라이언트 측에 노출시키지 않고 서버에서만 보관하고 계속해서 보안을 유지해야하는 것이 얼마나 중요한 것인지 뼈저리게 느끼게 되었습니다.`}
+                />
+            </MainContainer>
+            <MainContainer />
+        </Container>
     );
 }
 
